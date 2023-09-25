8cb41fb0276422310faaceb22c882ae90926c964
Yoon Hyeseong <80237040+hsyoonhs@users.noreply.github.com>
2023-09-25 11:38:49 +0900

    Add files via upload

---
+++ Day_06.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+	int a;
+	printf("두 자리 정수를 입력하세요 (10 ~ 99) ==> ");
+	scanf("%d", &a);
+	printf("%04d\t%04d\t\"%04d\"\t\'%04d\'\n\n", a, a, a, a);
+
+	// 정수형
+	/*
+	 * short (2 바이트 (16 비트))
+	 * int (4 바이트 (32 비트))
+	 * unsigned int (4 바이트)
+	 */
+
+	// 실수형
+	/*
+	 * float
+	 * asdf
+	 * asdf
+	 */
+
+	char b[10] = "012345678\0";
+	printf("%s \n", b);
+}
